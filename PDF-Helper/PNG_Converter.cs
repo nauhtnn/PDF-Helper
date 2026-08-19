b83c22b58a2aa20f364a3d5fa1077a2cb814e5c7
nauhtnn <thuan.nguyenngoc@live.com>
2026-08-19 13:48:25 +0700

    + Process all pages of PDF file.

---
+++ PDF-Helper/PNG_Converter.cs
@@ -0,0 +1,113 @@
+﻿using OpenCvSharp;
+using PDFtoImage;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PDF_Helper
+{
+    internal class PNG_Converter
+    {
+
+        static List<MemoryStream> LoadPDF_toMemory(string pdfPath)
+        {
+            var streams = new List<MemoryStream>();
+            byte[] bytes = File.ReadAllBytes(pdfPath);
+            var pageCount = Conversion.GetPageCount(bytes);
+            foreach(var page in Enumerable.Range(0, pageCount))
+            {
+                var stream = new MemoryStream();
+                using (SkiaSharp.SKBitmap bitmap = Conversion.ToImage(bytes, page))
+                using (var image = SkiaSharp.SKImage.FromBitmap(bitmap))
+                using (var data = image.Encode(SkiaSharp.SKEncodedImageFormat.Png, 100))
+                {
+                    data.SaveTo(stream);
+                }
+                streams.Add(stream);
+            }
+            
+            return streams;
+        }
+
+        static void CvThreshold(Mat img)
+        {
+            Cv2.Threshold(img, img, 0, 255, ThresholdTypes.Otsu);
+        }
+
+        public static List<byte[]> LoadFileForOCR(string filePath)
+        {
+            if(System.IO.File.Exists(filePath) == false)
+            {
+                Console.WriteLine("File not found: {0}", filePath);
+                return new List<byte[]>();  
+            }
+
+            List<Mat> images;
+            if(filePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase))
+            {
+                var streams = LoadPDF_toMemory(filePath);
+                images = CvPreprocessMemoryStreams(streams);
+            }
+            else
+            {
+                images = new List<Mat> { CvPreprocessImageFile(filePath) };
+            }
+
+            return images.Select(img => img.ToBytes()).ToList();
+        }
+
+        static Mat CvPreprocessImageFile(string filePath)
+        {
+            Mat image = Cv2.ImRead(filePath, ImreadModes.Grayscale);
+            CvThreshold(image);
+#if DEBUG
+            Cv2.ImWrite("processed.png", image);
+#endif
+            return image;
+        }
+
+        static List<Mat> CvPreprocessMemoryStreams(List<MemoryStream> streams)
+        {
+            var images = new List<Mat>();
+
+            foreach(var stream in streams)
+            {
+                Mat image = Cv2.ImDecode(stream.ToArray(), ImreadModes.Grayscale);
+                CvThreshold(image);
+                images.Add(image);
+            }
+            return images;
+        }
+
+        public static void Debug_SavePDF_to_PNG_File(string pdfPath, string outputDir)
+        {
+            Directory.CreateDirectory(outputDir);
+
+            byte[] bytes = File.ReadAllBytes(pdfPath);
+
+            int pageCount = Conversion.GetPageCount(bytes);
+            for (int page = 0; page < pageCount; page++)
+            {
+                using (SkiaSharp.SKBitmap bmp = Conversion.ToImage(bytes, page))
+                {
+                    // Save to file
+                    string filePath = Path.Combine(outputDir, $"page_{page + 1}.png");
+                    using (var image = SkiaSharp.SKImage.FromBitmap(bmp))
+                    using (var data = image.Encode(SkiaSharp.SKEncodedImageFormat.Png, 100))
+                    using (var stream = File.OpenWrite(filePath))
+                    {
+                        data.SaveTo(stream);
+                    }
+
+                    Console.WriteLine($"Saved to {filePath}");
+                }
+            }
+        }
+    }
+}
--- PDF-Helper/Program.cs
+++ PDF-Helper/Program.cs
@@ -8,102 +8,36 @@ namespace PDF_Helper
 {
     internal class Program
     {
-        static void Debug_SaveImageToFile(SkiaSharp.SKBitmap bitmap, string filePath)
-        {
-            using (var image = SkiaSharp.SKImage.FromBitmap(bitmap))
-            using (var data = image.Encode(SkiaSharp.SKEncodedImageFormat.Png, 100))
-            using (var stream = File.OpenWrite(filePath))
-            {
-                data.SaveTo(stream);
-            }
-        }
-        static void Debug_SavePdfToImages(string pdfPath, string outputDir)
-        {
-            Directory.CreateDirectory(outputDir);
-
-            byte[] bytes = File.ReadAllBytes(pdfPath);
-
-            int pageCount = Conversion.GetPageCount(bytes);
-            for (int page = 0; page < pageCount; page++)
-            {
-                using (SkiaSharp.SKBitmap bmp = Conversion.ToImage(bytes, page))
-                {
-                    // Save to file
-                    string filePath = Path.Combine(outputDir, $"page_{page + 1}.png");
-                    Debug_SaveImageToFile(bmp, filePath);
-                }
-            }
-
-            Console.WriteLine("PDF conversion complete!");
-        }
-
-        static MemoryStream LoadPDFtoMemory(string pdfPath)
-        {
-            byte[] bytes = File.ReadAllBytes(pdfPath);
-            var stream = new MemoryStream();
-            using (SkiaSharp.SKBitmap bitmap = Conversion.ToImage(bytes, 0))
-            using (var image = SkiaSharp.SKImage.FromBitmap(bitmap))
-            using (var data = image.Encode(SkiaSharp.SKEncodedImageFormat.Png, 100))
-            {
-                data.SaveTo(stream);
-            }
-            return stream;
-        }
+        
 
         static void Main(string[] args)
         {
             string pdfPath = "E:\\Dev\\PDF-Helper\\sample_vietnamese.pdf";
-#if DEBUG
-            string pngPath = "E:\\Dev\\PDF-Helper\\sample_vietnamese.png";
-            string outputDir = "E:\\Dev\\PDF-Helper\\output_images";
-            Debug_SavePdfToImages(pdfPath, outputDir);
-            OCR(pngPath);
-#else
             OCR(pdfPath);
-#endif
         }
         static void OCR(string filePath)
         {
-            Mat image;
-            if (filePath.EndsWith(".png"))
+            var images = PNG_Converter.LoadFileForOCR(filePath);
+            var resultText = new System.Text.StringBuilder();
+            int pageIndex = 0;
+            foreach (var image in images)
             {
-                image = Cv2.ImRead(filePath, ImreadModes.Grayscale);
-            }
-            else
-            {
-                byte[] imageBytes = LoadPDFtoMemory(filePath).ToArray();
-
-                // Decode image from byte array
-                image = Cv2.ImDecode(imageBytes, ImreadModes.Grayscale);
-            }
-
-            // Preprocess: thresholding for better OCR
-            Cv2.Threshold(image, image, 0, 255, ThresholdTypes.Otsu);
-
-            // Save preprocessed image (optional)
-#if DEBUG
-            Cv2.ImWrite("processed.png", image);
-#endif
-
-            // Initialize Tesseract with Vietnamese traineddata
-            using (var engine = new TesseractEngine(@"./tessdata", "vie", EngineMode.Default))
-            {
-#if DEBUG
-                using (var pix = Pix.LoadFromFile("processed.png"))
-#else
-
-                using (var pix = Pix.LoadFromMemory(image.ToBytes(".png")))
-#endif
+                // Initialize Tesseract with Vietnamese traineddata
+                using (var engine = new TesseractEngine(@"./tessdata", "vie", EngineMode.Default))
                 {
-                    using (var page = engine.Process(pix))
+                    using (var pix = Pix.LoadFromMemory(image))
                     {
-                        string text = page.GetText();
-                        Console.WriteLine("Recognized Text:");
-                        Console.WriteLine(text);
-                        File.WriteAllText("Vi-OCR.txt", text, System.Text.Encoding.UTF8);
+                        using (var page = engine.Process(pix))
+                        {
+                            resultText.AppendLine(page.GetText());
+                            Console.WriteLine("Recognized page {1} of file {0}.:", filePath, ++pageIndex);
+                        }
                     }
                 }
             }
+
+            var outputFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "_OCR.txt");
+            File.WriteAllText(outputFilePath, resultText.ToString(), System.Text.Encoding.UTF8);
         }
     }
 }
